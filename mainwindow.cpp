a5d37fdc36f3ef44c39612cb95f3ca62eda9701c
Bogdan Cristea <cristeab@gmail.com>
2016-06-12 21:26:10 +0300

    add plot

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,12 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    resize(548, 420);
+    QWidget *centralWidget= new QWidget(this);
+    QVBoxLayout *verticalLayout= new QVBoxLayout(centralWidget);
+    _customPlot = new QCustomPlot(centralWidget);
+    verticalLayout->addWidget(_customPlot);
+    setCentralWidget(centralWidget);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "qcustomplot.h"
 
 class MainWindow : public QMainWindow
 {
@@ -10,6 +11,8 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+private:
+    QCustomPlot *_customPlot;
 };
 
 #endif // MAINWINDOW_H
